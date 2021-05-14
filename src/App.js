8c355bd956214ce20c7f0882be99d75b1b09f6d7
Lee Ming De <leemingde@gmail.com>
2021-05-14 13:36:02 +0800

    Add Places items

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 
 import Users from './user/pages/Users';
-import NewPlace from './places/components/pages/NewPlace';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
     return (
-    <Router>
-        <MainNavigation />
-        <main>
-            <Switch>
-                <Route path="/" exact>
-                    <Users/>
-                </Route>
-                <Route path="/places/new" exact>
-                    <NewPlace />
-                </Route>
-                <Redirect to="/"/>
-            </Switch>
-        </main>
-    </Router>
-    );
+        <Router>
+            <MainNavigation />
+            <main>
+                <Switch>
+                    <Route path="/" exact>
+                        <Users/>
+                    </Route>
+                    <Route path="/:userId/places" exact>
+                        <UserPlaces />
+                    </Route>
+                    <Route path="/places/new" exact>
+                        <NewPlace />
+                    </Route>
+                    <Redirect to="/"/>
+                </Switch>
+            </main>
+        </Router>
+        );
 };
 
 export default App;
