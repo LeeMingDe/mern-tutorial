0d3fc6243779e953e4d893f60054654518fc1716
Lee Ming De <leemingde@gmail.com>
2021-05-17 16:04:52 +0800

    Set up places route

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/places', placesRoutes);
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route', 404);
+    next(error);
+});
 
 app.use((error, req, res, next) => {
     if (res.headerSent) {
         return next(error);
     }
-
     res.status(error.code || 500);
     res.json({message: error.message || 'An unknown error occurred'});
 });
