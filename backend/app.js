d489a30c0eff2e7a96afb43d51413ec42a3f0837
Lee Ming De <leemingde@gmail.com>
2021-05-22 13:28:44 +0800

    Tweaks for combined app

--- backend/app.js
+++ backend/app.js
@@ -13,21 +13,26 @@ const app = express();
 app.use(express.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use(express.static(path.join('public')));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+//     next();
+// });
 
 app.use('/api/users', usersRoutes);
 app.use('/api/places', placesRoutes);
 
 app.use((req, res, next) => {
-    next(new HttpError('Could not find this route', 404));
-});
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
+
+// app.use((req, res, next) => {
+//     next(new HttpError('Could not find this route', 404));
+// });
 
 app.use((error, req, res, next) => {
     if (req.file) {
